bc5c68258300eb2abb4198342dc0e31a4913101b
TimSelivan <timselivanson@gmail.com>
2023-08-26 19:34:08 +0300

    Implement filtration method for the wrong segment order rule

--- src/main/java/com/gridnine/testing/service/WrongSegmentsOrderFilter.java
+++ src/main/java/com/gridnine/testing/service/WrongSegmentsOrderFilter.java
@@ -4,11 +4,15 @@ import com.gridnine.testing.model.Flight;
 import com.gridnine.testing.model.Segment;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WrongSegmentsOrderFilter implements Filter {
 
     @Override
     public List<Segment> apply(List<Flight> flightsList) {
-        return null;
+        return flightsList.stream()
+                .flatMap(list -> list.getSegments().stream())
+                .filter(segment -> segment.getDepartureDate().isAfter(segment.getArrivalDate()))
+                .collect(Collectors.toList());
     }
 }
