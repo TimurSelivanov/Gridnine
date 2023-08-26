3bd54ae1464ae196f25e290d1d62061c7fd4723b
TimSelivan <timselivanson@gmail.com>
2023-08-26 21:52:44 +0300

    Refactor class LayoverTimeFilter

--- src/main/java/com/gridnine/testing/service/LayoverTimeFilter.java
+++ src/main/java/com/gridnine/testing/service/LayoverTimeFilter.java
@@ -3,12 +3,13 @@ package com.gridnine.testing.service;
 import com.gridnine.testing.model.Flight;
 import com.gridnine.testing.model.Segment;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LayoverTimeFilter implements Filter {
 
-    private final static int LAYOVER_TIME_LIMIT = 2; //TODO: move variable to a util or validation class
+    private final static long LAYOVER_TIME_LIMIT = 2; //TODO: move variable to a util or validation class
 
     @Override
     public List<Flight> apply(List<Flight> flightsList) {
@@ -18,11 +19,11 @@ public class LayoverTimeFilter implements Filter {
     }
 
     private static boolean isLayoverTimeMoreThanLimit(Flight flight) {
-        int calculatedLayoverTime = 0;
+        long calculatedLayoverTime = 0;
         List<Segment> segments = flight.getSegments();
-        for(int i = 1; i < segments.size(); i++) {
-            calculatedLayoverTime += (segments.get(i).getDepartureDate().getHour() - segments.get(i - 1).getArrivalDate().getHour());
-        }
+        for(int i = 1; i < segments.size(); i++)
+            calculatedLayoverTime += Duration.between(segments.get(i - 1).getArrivalDate(), segments.get(i).getDepartureDate())
+                    .toHours();
         return calculatedLayoverTime > LAYOVER_TIME_LIMIT;
     }
 }
